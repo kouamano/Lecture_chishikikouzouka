f3dd18393c2baba5d5a3d58429528d5c93b31f77
kouamano <amano.au1@gmail.com>
2013-10-24 10:44:02 +0900

    new

---
+++ program/seq3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int fib(int n){
+	int out;
+	if(n == 0){
+		out = -1;
+	}else if(n == 1){
+		out = 1;
+	}else if(n == 2){
+		out = -1;
+	}else{
+		out = (fib(n-1) + fib(n-2)) * fib(n-3);
+	}
+	return(out);
+}
+
+int main(int argc, char **argv){
+	int m;
+	sscanf(argv[1],"%d",&m);
+	printf("%d\n",fib(m));
+}
